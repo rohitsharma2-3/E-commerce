0ea6b435ab9a9f5833b5d811d6178dcac4b23dbb
Rohit Sharma <rohit.sharma2k2k@gmail.com>
2025-07-13 13:15:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,10 @@ const verifyToken = require('./middleware/Verify')
 const port = process.env.PORT || 4000
 app.use(express.json());
 
-app.use(cors())
+app.use(cors({
+  origin: 'https://ecommerce-t9fl.onrender.com', // your frontend
+  credentials: true
+}))
 app.use(cookie())
 
 app.get('/', (req, res) => {
