a76c95787ba02fd8b333e030fe26c477c0d044b6
Rohit Sharma <rohit.sharma2k2k@gmail.com>
2025-07-20 19:16:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,12 +25,13 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: allowedOrigins,
-  credentials: true
-}));
-
-app.options('*', cors({
-  origin: allowedOrigins,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS: ' + origin));
+    }
+  },
   credentials: true
 }));
 
